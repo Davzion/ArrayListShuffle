7d2af970d52b0f78cd7c37d6e25e9661d405df19
Davzion <dmontgomery06@att.net>
2021-04-30 21:20:26 -0400

    Final Commit

--- src/ListMainClass.java
+++ src/ListMainClass.java
@@ -4,10 +4,16 @@ import java.util.Collections;
 public class ListMainClass {
     public static void main(String[] args) {
         ArrayList<Integer> arrayListOfIntegers = new ArrayList();
-        arrayListOfIntegers.add(1);
-        arrayListOfIntegers.add(2);
-        arrayListOfIntegers.add(3);
-        arrayListOfIntegers.add(4);
+        arrayListOfIntegers.set(1,1);
+        arrayListOfIntegers.set(2,2);
+        arrayListOfIntegers.set(3,3);
+        arrayListOfIntegers.add(5);
+
+        arrayListOfIntegers.contains(2);
+        arrayListOfIntegers.set(5,6);
+        arrayListOfIntegers.retainAll(arrayListOfIntegers);
+        arrayListOfIntegers.remove(5);
+
 
         System.out.println(arrayListOfIntegers);
         Collections.shuffle(arrayListOfIntegers);
